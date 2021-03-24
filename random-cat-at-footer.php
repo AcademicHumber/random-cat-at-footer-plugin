13d331df72223619472f3359a4f6f720a58af3aa
Humber <ahfernandez-es@udabol.edu.bo>
2021-03-24 10:29:35 -0400

    Archivo inicial del plugin

---
+++ random-cat-at-footer.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 
+ * @link              https://www.linkedin.com/in/adrian-fernandez-1701/
+ * @since             1.0.0
+ * @package           Random Cat At Footer
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Random Category at Footer
+ * Plugin URI:        https://github.com/AcademicHumber/order-reminder-plugin
+ * Description:       Show random category posts at the end of category page.
+ * Version:           1.0.0
+ * Author:            Adrian Fernandez
+ * Author URI:        https://www.linkedin.com/in/adrian-fernandez-1701/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       random-cat-at-footer
+ */
